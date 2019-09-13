c418fa3bd4e9275ac20bb2d9b41cbba230aedf4d
Haitham <hm.elshenawy@gmail.com>
2019-09-13 07:32:19 +0200

    Initial commit

---
+++ app/src/main/java/com/haitham/cafemanagment/Items_DataBase_Adaptor.java
@@ -0,0 +1,136 @@
+package com.haitham.cafemanagment;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class Items_DataBase_Adaptor {
+
+    Items_DB_helper mhelper;
+    SQLiteDatabase mSQLiteDB;
+    ArrayList<Drink_class> items_ArrayList;
+
+
+    public Items_DataBase_Adaptor(Context context) {
+        mhelper = new Items_DB_helper(context);
+        mSQLiteDB = mhelper.getWritableDatabase();
+    }
+
+    public void insertData(String itemName, double price, int quantity, int coffee, int milk, int suger) {
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(mhelper.COLUMN_ITEMNAME, itemName);
+        contentValues.put(mhelper.COLUMN_PRICE, price);
+        contentValues.put(mhelper.COLUMN_QUANTITY, quantity);
+        contentValues.put(mhelper.COLUMN_COFFEE_UNITS, coffee);
+        contentValues.put(mhelper.COLUMN_MILK_UNITS, milk);
+        contentValues.put(mhelper.COLUMN_SUGER_UNITS, suger);
+        long l = mSQLiteDB.insert(mhelper.TABLE_NAME, null, contentValues);
+
+    }
+
+    public ArrayList<Drink_class> getAllData() {
+
+        items_ArrayList = new ArrayList<>();
+        String[] columns = {mhelper.UID , mhelper.COLUMN_ITEMNAME,mhelper.COLUMN_QUANTITY, mhelper.COLUMN_PRICE,
+                                mhelper.COLUMN_COFFEE_UNITS, mhelper.COLUMN_MILK_UNITS, mhelper.COLUMN_SUGER_UNITS };
+        Cursor cursor = mSQLiteDB.query(mhelper.TABLE_NAME, columns, null, null, null, null, null);
+
+        while (cursor.moveToNext()) {
+
+            long item_ID = cursor.getLong(0);
+            String itemName = cursor.getString(1);
+            int quantity = cursor.getInt(2);
+            double price = cursor.getDouble(3);
+            int coffee = cursor.getInt(4);
+            int milk = cursor.getInt(5);
+            int suger = cursor.getInt(6);
+
+            Drink_class object = new Drink_class();
+            object.id = item_ID;
+            object.item_name = itemName;
+            object.price = price;
+            object.quantity = quantity;
+            object.Coffee_Unit = coffee;
+            object.Milk_Unit = milk;
+            object.Suger_Unit = suger;
+
+            items_ArrayList.add(object);
+        }
+        return items_ArrayList;
+    }
+
+    public void removeAllData_FirstTable() {
+
+        String delete = "DELETE FROM " + mhelper.TABLE_NAME;
+        mSQLiteDB.execSQL(delete);
+    }
+
+    public void removeRow_FirstTable(long id){
+
+        mSQLiteDB.delete(mhelper.TABLE_NAME,mhelper.UID + " = "+ id, null  );
+    }
+
+    class Items_DB_helper extends SQLiteOpenHelper {
+
+        private static final String DATABASE_NAME = "SHOP_DATABASE";
+        private static final String TABLE_NAME = "FIRST_TABLE";
+        private static final int DATABASE_VER = 1;
+        private static final String UID = "ID";
+        private static final String COLUMN_ITEMNAME = "Item";
+        private static final String COLUMN_QUANTITY = "Quantity";
+        private static final String COLUMN_PRICE = "Price";
+        private static final String COLUMN_COFFEE_UNITS = "CoffeeUnits";
+        private static final String COLUMN_MILK_UNITS = "MilkUnits";
+        private static final String COLUMN_SUGER_UNITS = "SugerUnits";
+        private static final String CREATE_FIRST_TABLE = " CREATE TABLE " + TABLE_NAME
+                + " ( "
+                + UID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + COLUMN_ITEMNAME + " VARCHAR(255), "
+                + COLUMN_QUANTITY + " VARCHAR(255), "
+                + COLUMN_PRICE + " VARCHAR(255),"
+                + COLUMN_COFFEE_UNITS + " VARCHAR(255),"
+                + COLUMN_MILK_UNITS + " VARCHAR(255), "
+                + COLUMN_SUGER_UNITS + " VARCHAR(255));";
+
+        private static final String DROP_FIRST_TABLE = "  DROP TABLE IF EXISTS " + TABLE_NAME + "; ";
+        Context mContext;
+
+
+
+
+        public Items_DB_helper(@Nullable Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VER);
+            this.mContext = context;
+
+        }
+
+
+        @Override
+        public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+            try {
+                sqLiteDatabase.execSQL(CREATE_FIRST_TABLE);
+            } catch (SQLException e) {
+                System.out.println("SQL Exception");
+            }
+        }
+
+
+        @Override
+        public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+            sqLiteDatabase.execSQL(DROP_FIRST_TABLE);
+            onCreate(sqLiteDatabase);
+
+        }
+    }
+}
